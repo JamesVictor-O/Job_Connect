d7e43b38f027905e042685f72c4d5c3dc04d8e88
unknown <victorjames40v@gmail.com>
2023-08-09 21:37:58 +0100

    worked on the mobile navbar

---
+++ index.js
@@ -0,0 +1,17 @@
+let humbugerEl = document.querySelector(".humbuger");
+let mobileMenuEl = document.querySelector(".mobilemenu");
+
+function OpenCloseHumbuger(humbuger) {
+    humbuger.addEventListener("click", () => {
+        humbuger.classList.toggle("is-active")
+        openCloseNavbar(humbugerEl,mobileMenuEl)
+    })
+}
+function openCloseNavbar(humbuger, navMenu) {
+    if (humbuger.classList.contains("is-active")) {
+        navMenu.classList.add("la-active")
+    } else {
+        navMenu.classList.remove("la-active")
+    }    
+}
+OpenCloseHumbuger(humbugerEl)
