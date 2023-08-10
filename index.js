dbbbd77fd00ddd66d01fb578956f6018201ecdd4
unknown <victorjames40v@gmail.com>
2023-08-10 13:47:33 +0100

    worked on the sign up user interface

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 let humbugerEl = document.querySelector(".humbuger");
 let mobileMenuEl = document.querySelector(".mobilemenu");
+let signUpButtonEl = document.querySelector("#signup")
+const signUpPagePg = document.querySelector("#signUpPage");
+const homePagePg = document.querySelector(".homepage");
+
+
+
 
 function OpenCloseHumbuger(humbuger) {
     humbuger.addEventListener("click", () => {
@@ -16,3 +22,14 @@ function openCloseNavbar(humbuger, navMenu) {
     }    
 }
 OpenCloseHumbuger(humbugerEl)
+
+// sign up and login page
+
+function signUp(button) {
+    console.log(button)
+    button.addEventListener("click", () => {
+        signUpPagePg.style.display = "flex"
+        homePagePg.style.display="none"
+    })
+}
+signUp(signUpButtonEl)
