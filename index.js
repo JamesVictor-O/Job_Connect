48cc2fbce0bc691c079ee57398ffab3b216e1f9f
unknown <victorjames40v@gmail.com>
2023-08-11 00:08:44 +0100

    worked on form validation functionality

--- index.js
+++ index.js
@@ -26,10 +26,43 @@ OpenCloseHumbuger(humbugerEl)
 // sign up and login page
 
 function signUp(button) {
-    console.log(button)
     button.addEventListener("click", () => {
         signUpPagePg.style.display = "flex"
         homePagePg.style.display="none"
     })
 }
 signUp(signUpButtonEl)
+
+function error(input) {
+        input.classList.add("error")
+}
+function success(input) {
+        input.classList.add("success")
+}
+
+function validating(input) {
+    if (input.value === "") {
+        error(input)
+    } else (
+        success(input)
+    )
+}
+// form validation
+function formValidation() {
+    const inputFirstName=document.querySelector("#firstname")
+    const inputLastName=document.querySelector("#secondname")
+    const inputEmail=document.querySelector("#email")
+    const inputPhoneNumber=document.querySelector("#phoneNo")
+    const inputPassword=document.querySelector("#password")
+    const inputConfirmPassword=document.querySelector("#confirmPassword")
+    const inputIdentity=document.querySelector("#idntity")
+    
+    const signUpBotton = document.querySelector("#formSignUp");
+    signUpBotton.addEventListener("click", () => {
+        validating(inputFirstName)
+        validating(inputLastName)
+        validating(inputEmail)
+        validating(inputPhoneNumber)
+    })
+}
+formValidation()
